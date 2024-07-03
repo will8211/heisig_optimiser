99d4dd2859f221b8edefc877f60f3eb0e7acb42a
will8211 <wmannard@gmail.com>
2024-07-03 08:45:58 -0400

    add d3 demo

---
+++ d3test/app.js
@@ -0,0 +1,96 @@
+// app.js
+
+const width = 1200;
+const height = 800;
+
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+// Define the Nodes and Links
+
+const nodes = [
+  {
+    id: "G",
+    label: "占\ntell fortunes (44)",
+    background: "./assets/Medium.png",
+  },
+  {
+    id: "E",
+    label: "卜\ndivination (43)",
+  },
+  {
+    id: "C",
+    label: "丨\nwalking stick (primitive)",
+  },
+  { id: "B", label: "一\none (1)", background: "./assets/Elementary.png" },
+  {
+    id: "D",
+    label: "丶\na drop of (primitive)",
+  },
+  {
+    id: "F",
+    label: "口\nmouth (11)",
+    background: "./assets/Elementary.png",
+  },
+];
+
+const links = [
+  { source: "E", target: "G", thickness: 2 },
+  { source: "C", target: "E", thickness: 2 },
+  { source: "B", target: "C", thickness: 2 },
+  { source: "D", target: "E", thickness: 2 },
+  { source: "F", target: "G", thickness: 2 },
+];
+
+// Create and Style Nodes and Links
+
+const simulation = d3.forceSimulation(nodes)
+  .force("link", d3.forceLink(links).id(d => d.id).distance(200))
+  .force("charge", d3.forceManyBody().strength(-500))
+  .force("center", d3.forceCenter(width / 2, height / 2));
+
+const link = svg.append("g")
+  .selectAll("line")
+  .data(links)
+  .enter().append("line")
+  .attr("class", "link")
+  .attr("stroke-width", d => d.thickness);
+
+const node = svg.append("g")
+  .selectAll("g")
+  .data(nodes)
+  .enter().append("g");
+
+node.append("circle")
+  .attr("r", 40)
+  .attr("fill", d => `url(#${d.id}-img)`);
+
+node.append("text")
+  .attr("dy", -5)
+  .attr("text-anchor", "middle")
+  .text(d => d.label)
+  .style("font-size", "15px");
+
+node.append("pattern")
+  .attr("id", d => `${d.id}-img`)
+  .attr("patternUnits", "userSpaceOnUse")
+  .attr("width", 80)
+  .attr("height", 80)
+  .append("image")
+  .attr("xlink:href", d => d.background)
+  .attr("width", 80)
+  .attr("height", 80);
+
+simulation.on("tick", () => {
+  link
+    .attr("x1", d => d.source.x)
+    .attr("y1", d => d.source.y)
+    .attr("x2", d => d.target.x)
+    .attr("y2", d => d.target.y);
+
+  node
+    .attr("transform", d => `translate(${d.x},${d.y})`);
+});
