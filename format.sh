752fbd60a44ed2c3233e0f3d44eb784ff1b7e60c
wmannard <wmannard@coveo.com>
2024-07-07 17:31:17 -0400

    formatting

--- format.sh
+++ format.sh
@@ -5,11 +5,7 @@ poetry run python -m isort heisig_optimiser
 poetry run python -m black heisig_optimiser
 
 echo "Formatting jinja files..."
-poetry run python -m djlint 'template/index.jinja' 'template/template.jinja' \
-    --extension="jinja" \
-    --profile="jinja" \
-    --reformat \
-    --indent 2
+poetry run python -m djlint 'template/index.jinja' 'template/template.jinja' --reformat
 
 echo "Formatting js and css files..."
 poetry run python -m prettier \
